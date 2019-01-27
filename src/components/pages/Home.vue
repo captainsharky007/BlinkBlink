4fa4e716071424bcd715eb47f61ff5288b610db8
Jevgenijs Zaremba <jevgenijs@zaremba.lv>
2019-01-27 19:48:02 +0200

    Initial commit

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,209 @@
+<template>
+  <main class="bg-light">
+
+    <div class="container mb-32 mt-2">
+      <div class="row">
+        <div class="col">
+           <img src="~@/assets/img/blink-blink-dark.svg" alt="BlinkBlink Eye training application">
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="display-4 mb-16">Learn to take care of your eyes.</h1>
+          <p class="mb-24">Reduce eyestrain, dry eyes and headaches with guided eye relaxation exercises.</p>
+          <h2 class="mb-32">Only €4.99 per year.</h2>
+          <a class="btn btn-block btn-lg btn-primary" href="#" role="button">Start Free Trial</a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col p-0">
+          <img class="w-100" src="~@/assets/img/guy-hero@2x.jpg" alt="Guy">
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row mt-64">
+        <div class="col">
+          <h1 class="mb-16">Less stress<br> and more clarity.</h1>
+          <h1 class="mb-16">Just 7 minutes a day.</h1>
+          <p class="mb-32">Choose from curated exercise plans, designed with the help of experienced ophthalmologists.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6 benefit mb-32">
+          <img src="~@/assets/img/icons/circulate.svg" alt="Circulate">
+          <h2 class="mt-16">Improved blood circulation</h2>
+        </div>
+        <div class="col-6 benefit">
+          <img src="~@/assets/img/icons/flex.svg" alt="Flex">
+          <h2 class="mt-16">Improved<br> eye<br> flexibility</h2>
+        </div>
+        </div><div class="row mb-64">
+        <div class="col-6 benefit">
+          <img src="~@/assets/img/icons/diamond.svg" alt="Diamond">
+          <h2 class="mt-16">Stronger focus</h2>
+        </div>
+        <div class="col-6 benefit">
+          <img src="~@/assets/img/icons/eye-strain.svg" alt="Eye strain">
+          <h2 class="mt-16">Reduced<br> eye strain</h2>
+        </div>
+      </div>
+      <div class="row mb-64">
+        <div class="col">
+          <img
+            class="p-2"
+            width="100%"
+            src="~@/assets/img/phone-exercises@2x.png"
+            alt="Eye Exercises."
+          >
+        </div>
+      </div>
+    </div>
+
+    <div class="container bg-primary text-white mb-64">
+      <div class="row">
+        <div class="col">
+          <h1 class="mt-64 mb-16">Your personal eye trainer in your pocket.</h1>
+          <p class="mb-32 text-white">Follow our animated exercise guide and learn how to relax your eyes in just a few simple steps.</p>
+          <img
+            class="p-2 mb-32"
+            width="100%"
+            src="~@/assets/img/phone-guide@2x.png"
+            alt="Eye training guide."
+          >
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="mb-16">Grow the habit.
+            <br>Stay motivated.
+          </h1>
+          <p class="mb-32">With progress tracking, achievements and reminders, we’ll make sure you are not going to quit.</p>
+          <img
+            class="p-2 mb-64"
+            width="100%"
+            src="~@/assets/img/phone-alerts@2x.png"
+            alt="Eye training remainders."
+          >
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="mb-32">Thousands of people aleady report reduced eyestrain and even improved eyesight only after a week of persistent practice.</h1>
+        </div>
+      </div>
+      <div class="row mb-128">
+        <div class="col p-0">
+          <div class="user-wrapper">
+            <div class="user">
+              <img src="~@/assets/img/user-1.png" alt="User 1">
+            </div>
+            <div class="user">
+              <img src="~@/assets/img/user-2.png" alt="User 2">
+            </div>
+            <div class="user">
+              <img src="~@/assets/img/user-3.png" alt="User 3">
+            </div>
+            <div class="user">
+              <img src="~@/assets/img/user-4.png" alt="User 4">
+            </div>
+            <div class="user">
+              <img src="~@/assets/img/user-5.png" alt="User 5">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container mb-128">
+      <div class="row">
+        <div class="col">
+          <h1 class="display-4 mb-32">Give your eyes a break. Start today.</h1>
+          <h2 class="mb-32">Only €4.99 per year.</h2>
+          <a class="btn btn-block btn-lg btn-primary" href="#" role="button">Start Free Trial</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h2 class="text-gray mb-16">Can eye exercises
+            <br>improve vision?
+          </h2>
+          <p
+            class="body-small"
+          >There is a lot of debate as to whether eye exercises can really improve your vision.</p>
+          <p
+            class="body-small"
+          >There aren’t any scientific studies that show eye exercises can successfully restore your vision, but there’s a lot of hearsay evidence to the contrary, with countless people saying exercises have significantly improved their eyesight.</p>
+          <p
+            class="body-small mb-64"
+          >It's possible, that poor vision, headaches and dry eyes might be a sign of another medical condition that requires treatment. This is something that eye relaxation cannot account for, so please visit your doctor first.</p>
+        </div>
+      </div>
+    </div>
+
+    <footer class="bg-dark">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <img class="mt-32 mb-64" src="~@/assets/img/blink-blink-gray.svg" alt="BlinkBlink Eye training application">
+          </div>
+        </div>
+         <div class="row">
+          <div class="col text-white">
+            <h3 class="mb-32" style="font-size:1rem; line-height:20px;">Contact Us</h3>
+            <h3 class="mb-32" style="font-size:1rem; line-height:20px;">Privacy & Terms</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col mb-16">
+            <small>© 2019 blink blink app</small>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </main>
+</template>
+
+<style>
+.benefit > h2 {
+  line-height: initial;
+}
+.user-wrapper {
+  justify-content: space-between;
+  display: flex;
+}
+.user {
+  display: inline-block;
+  /* width: 16%; */
+}
+.user img {
+  width: 100%;
+  padding-left: 16px;
+}
+.user-1 {
+  background-image: url("~@/assets/img/user-1.png");
+}
+</style>
+
+
+<script>
+import About from "@/components/templates/About";
+export default {
+  components: {
+    About
+  }
+};
+</script>
---
+++ src/components/pages/Style.vue
@@ -0,0 +1,59 @@
+<template>
+  <main>
+    <div class="container mb-96 mt-96">
+      <div class="row mb-32">
+        <div class="col-12 mr-auto">
+          <div class="display-4">Title</div>
+          <h1>Heading 1</h1>
+          <h2>Heading 2</h2>
+          <h3>Heading 3</h3>
+          <p>Body</p>
+          <p class="body-small">Body small</p>
+          <small>Caption</small>
+        </div>
+      </div>
+      <div class="row mb-32">
+        <div class="col">
+          <a class="btn btn-lg btn-primary" href="#" role="button">Button</a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="d-inline-block mr-16">
+            <div style="width:100px; height:100px;" class="bg-dark"></div>Dark
+            <br>#063642
+          </div>
+         <div class="d-inline-block mr-16">
+            <div style="width:100px; height:100px; background-color:#586e75"></div>Gray
+            <br>#586e75
+          </div>
+           <div class="d-inline-block mr-16">
+            <div style="width:100px; height:100px;" class="bg-primary"></div>Primary
+            <br>#6C71C4
+          </div>
+          <div class="d-inline-block mr-16">
+            <div style="width:100px; height:100px;" class="bg-light"></div>Light
+            <br>#FDF6E3
+          </div>
+        </div>
+         
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+// import ContactForm from '@/components/templates/ContactForm'
+
+// export default {
+//   components: {
+//     ContactForm
+//   },
+// beforeCreate: function () {
+//   document.querySelector('body').classList.add('bg-dark')
+// },
+// destroyed: function () {
+//   document.querySelector('body').classList.remove('bg-dark')
+// }
+// }
+</script>
